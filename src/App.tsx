727af87e4ec8cc01e3e6a87a28325a44dc45a7f0
Oleh Levchenko <leva13007@gmail.com>
2025-05-29 20:50:32 +0100

    01 create game Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
+import './App.css';
+import { grid } from './service/mock';
 
 function App() {
-  console.log("Render App");
+  console.log("Render App", grid);
   return (
-    <>
-      <h1>Tetris</h1>
-    </>
+    <main className='game'>
+      <section className="game__grid">
+        {
+          grid.map((row, i) => (
+            <div key={i} className="grid__row">
+              {
+                row.map((cell, j) => (
+                  <div key={`${i} ${j}`} className={`grid__cell ${cell ? 'grid__cell--active' : ''}`}></div>
+                ))
+              }
+            </div>
+          ))
+        }
+      </section>
+      <section className="game__info"></section>
+    </main>
   )
 }
 
